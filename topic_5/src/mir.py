50c3fce397dbd52b5d8c3cc56e096f7b20d72c0c
nboj <60493251+nboj@users.noreply.github.com>
2025-10-08 17:01:13 -0400

    u

--- topic_5/src/mir.py
+++ topic_5/src/mir.py
@@ -4,6 +4,7 @@ from .tokenizer import Token, TokenType
 from .types import NIL, Nil, Op, Symbol, SymbolType
 from .binder import (
     BoundAssign,
+    BoundAugAssign,
     BoundBinary,
     BoundCallExpr,
     BoundExpr,
@@ -13,8 +14,10 @@ from .binder import (
     BoundLetStmt,
     BoundLiteral,
     BoundProgram,
+    BoundReturnStmt,
     BoundStmt,
     BoundVariable,
+    BoundWhileStmt,
 )
 
 
@@ -25,7 +28,7 @@ class MStmt:
 
 @dataclass(frozen=True)
 class TermStmt:
-    pass
+    succ: list["Block"]
 
 
 @dataclass(frozen=True)
@@ -33,14 +36,16 @@ class Branch(TermStmt):
     condition: "Operand"
     true_block_id: int
     false_block_id: int
+    succ: list["Block"]
 
 @dataclass(frozen=True)
 class Goto(TermStmt):
     block_id: int
+    succ: list["Block"]
 
 @dataclass(frozen=True)
 class Halt(TermStmt):
-    pass
+    succ: list["Block"]
 
 @dataclass(frozen=True)
 class Place:
@@ -116,6 +121,9 @@ class Assign(MStmt):
     lval: Place
     rval: RValue | Operand
 
+@dataclass(frozen=True)
+class Return(MStmt):
+    val: Operand | RValue
 
 @dataclass(frozen=True)
 class Phi(RValue):
@@ -126,11 +134,13 @@ class Block:
     id: int
     stmts: list[MStmt]
     term: TermStmt
+    preds: list["Block"]
 
     def __init__(self, id) -> None:
         self.id = id
         self.stmts = []
-        self.term = Halt()
+        self.term = Halt([])
+        self.preds = []
 
 
 class CFGBuilder:
@@ -226,12 +236,12 @@ class CFGBuilder:
                 true_block = self.new_block()
                 false_block = self.new_block()
                 lhs = self.lower_to_operand(expr.left)
-                self.current_block().term = Branch(lhs, true_block.id, false_block.id)
+                self.current_block().term = Branch(condition=lhs, true_block_id=true_block.id, false_block_id=false_block.id, succ=[true_block, false_block])
                 self.blocks.append(true_block)
                 rhs = self.lower_to_operand(expr.right)
-                self.current_block().term = Goto(join_block.id)
+                self.current_block().term = Goto(block_id=join_block.id, succ=[join_block])
                 self.blocks.append(false_block)
-                self.current_block().term = Goto(join_block.id)
+                self.current_block().term = Goto(block_id=join_block.id, succ=[join_block])
                 self.blocks.append(join_block)
                 tmp = self.new_temp()
                 phi = Phi([(true_block.id, rhs), (false_block.id, Const(False))])
@@ -303,6 +313,9 @@ class CFGBuilder:
                 val = self.lower_to_operand(arg)
                 args.append(val)
             return Call(target, args)
+        elif isinstance(expr, BoundVariable):
+            place = self.ensure_local(expr.sym)
+            return Copy(place)
         elif isinstance(expr, BoundAssign):
             lval = self.lower_to_local(expr.target)
             rval = self.lower_expression(expr.value)
@@ -327,8 +340,27 @@ class CFGBuilder:
             place = self.lower_to_local(assign.target)
             value = self.lower_expression(assign.value)
             self.emit(Assign(place, value))
+        elif isinstance(stmt, BoundReturnStmt):
+            val = None
+            if stmt.expr is not None:
+                val = self.lower_expression(stmt.expr)
+            else:
+                val = Const(NIL)
+            self.emit(Return(val))
         elif isinstance(stmt, BoundExprStmt):
-            if isinstance(stmt.expr, BoundAssign):
+            if isinstance(stmt.expr, BoundAugAssign):
+                value = None
+                if stmt.expr.op.kind == TokenType.PlusEq:
+                    lhs = self.lower_to_operand(stmt.expr.target)
+                    rhs = self.lower_to_operand(stmt.expr.value)
+                    op = Op.ADD
+                    value = BinOp(op, lhs, rhs)
+                else:
+                    raise AssertionError(f"unhandled op kind in bound aug assign lowering: {stmt.expr.op.kind}")
+
+                lval = self.lower_to_local(stmt.expr.target)
+                self.emit(Assign(lval, value))
+            elif isinstance(stmt.expr, BoundAssign):
                 _ = self.lower_expression(stmt.expr)
             else: 
                 rval = self.lower_expression(stmt.expr)
@@ -339,15 +371,28 @@ class CFGBuilder:
             true_block = self.new_block()
             false_block = self.new_block()
             exit_block = self.new_block()
-            self.current_block().term = Branch(condition, true_block.id, false_block.id)
+            self.current_block().term = Branch(condition=condition, true_block_id=true_block.id, false_block_id=false_block.id, succ=[true_block, false_block])
             self.blocks.append(true_block)
             for s in stmt.then_block.stmts:
                 self.lower_stmt(s)
-            self.current_block().term = Goto(exit_block.id)
+            self.current_block().term = Goto(block_id=exit_block.id, succ=[exit_block])
             self.blocks.append(false_block)
             for s in stmt.else_block.stmts:
                 self.lower_stmt(s)
-            self.current_block().term = Goto(exit_block.id)
+            self.current_block().term = Goto(block_id=exit_block.id, succ=[exit_block])
+            self.blocks.append(exit_block)
+        elif isinstance(stmt, BoundWhileStmt):
+            head_block = self.new_block()
+            body_block = self.new_block()
+            exit_block = self.new_block()
+            self.current_block().term = Goto(succ=[head_block], block_id=head_block.id)
+            self.blocks.append(head_block)
+            condition = self.lower_to_operand(stmt.condition)
+            self.current_block().term = Branch(succ=[body_block, exit_block], condition=condition, true_block_id=body_block.id, false_block_id=exit_block.id)
+            self.blocks.append(body_block)
+            for s in stmt.block.stmts:
+                self.lower_stmt(s)
+            self.current_block().term = Goto(succ=[head_block], block_id=head_block.id)
             self.blocks.append(exit_block)
         else:
             raise AssertionError(f"Unhandeled expr type in lowerstmt mir {type(stmt)}")
@@ -356,6 +401,15 @@ class CFGBuilder:
         self.blocks.append(self.new_block())
         for stmt in self.fn.body.stmts:
             self.lower_stmt(stmt)
+        preds: dict[int, list[Block]] = {}
+        for block in self.blocks:
+            for succ in block.term.succ:
+                if not preds.get(succ.id):
+                    preds[succ.id] = []
+                preds[succ.id].append(block)
+        for block in self.blocks:
+            block.preds = preds.get(block.id) or []
+
 
 class MIRContext:
     globals: dict[int, Global]
@@ -382,11 +436,128 @@ class MIRContext:
             return local
         assert(False), "Invalid symbol type for a global local"
 
+
+class DominatorNode:
+    parent: "DominatorNode | None"
+    block_ref: Block | None
+    children: list["DominatorNode"]
+
+    def __init__(self, block_ref: Block | None, parent: "DominatorNode | None" = None) -> None:
+        self.parent = parent
+        self.children = []
+        self.block_ref = block_ref
+
+class DomTreeBuilder:
+    cfg: CFGBuilder
+    entry: DominatorNode
+
+    def __init__(self, cfg: CFGBuilder) -> None:
+        self.cfg = cfg
+        self.entry = DominatorNode(None)
+
+    def get_dominators(self, node: Block):
+        dominators = []
+        for pred in node.preds:
+            if pred is self.entry:
+                dominators.append(self.entry)
+                return dominators
+
+    def dominates(self, node: Block, other: Block):
+        for pred in node.preds:
+            pass
+
+    def build(self):
+        if len(self.cfg.blocks) <= 0:
+            return
+        map: dict[int, Block] = {}
+        self.entry.block_ref = self.cfg.blocks[0]
+        for block in self.cfg.blocks:
+            map[block.id] = block
+
+        dominator_nodes = {self.entry.block_ref.id: self.entry}
+
+        def get_dominator(block:Block):
+            if len(block.preds) == 1:
+                dom_node = dominator_nodes.get(block.preds[0].id)
+                if not dom_node:
+                    dom_node = DominatorNode(block.preds[0])
+                    dominator_nodes[block.preds[0].id] = dom_node
+                return dom_node
+            elif len(block.preds) == 0:
+                dom_node = self.entry
+                return dom_node
+            else:
+                preds = block.preds
+                while True:
+                    doms = []
+                    for node in preds:
+                        dominator = get_dominator(node)
+                        doms.append(dominator)
+                    prev = None
+                    same = True
+                    for dom in doms:
+                        if prev is None:
+                            prev = dom
+                        elif prev != dom:
+                            same = False
+                            break
+                    new_preds = []
+                    if same == True:
+                        return dominator_nodes.get(preds[0].id)
+                    else:
+                        for pred in preds:
+                            new_preds += pred.preds
+                        preds = new_preds
+
+        def compute_node(block: Block):
+            node = DominatorNode(block)
+            dominator = get_dominator(block)
+            assert dominator is not None
+            node.parent = dominator
+            dominator.children.append(node)
+            #if dominator_nodes.get(block.id) is None:
+            dominator_nodes[block.id] = node
+            return node
+
+        def compute_tree(current_node: Block):
+            root = compute_node(current_node)
+            for succ in current_node.term.succ:
+                _ = compute_tree(succ)
+            return root
+
+        self.entry = compute_tree(self.entry.block_ref)
+
+        def print_node(node: DominatorNode):
+            assert node.block_ref is not None
+            assert node.parent is not None
+            assert node.parent.block_ref is not None
+            print()
+            print("BLOCKID:", node.block_ref.id)
+            print("DOMINATOR:", node.parent.block_ref.id)
+            tmp = "CHILDREN: ["
+            for child in node.children:
+                assert child.block_ref is not None
+                tmp += str(child.block_ref.id) + ", "
+            if len(node.children) > 0:
+                tmp = tmp[0:-2]
+            tmp += "]"
+            print(tmp)
+            print()
+
+        def print_tree(node: DominatorNode):
+            print_node(node)
+            for child in node.children:
+                print_node(child)
+        print_tree(self.entry);
+
+
+
 class Mir:
     program: BoundProgram
     temp_counter: int
     debug: bool
     cfgs: list[CFGBuilder]
+    dom_trees: list[DomTreeBuilder]
     context: MIRContext
 
     def __init__(self, bound_program: BoundProgram, debug: bool = False) -> None:
@@ -394,6 +565,7 @@ class Mir:
         self.temp_counter = 0
         self.debug = debug
         self.cfgs = []
+        self.dom_trees = []
         self.context =  MIRContext()
 
     def build_cfg(self):
@@ -404,8 +576,15 @@ class Mir:
             builder.build()
             self.cfgs.append(builder)
 
+    def build_trees(self):
+        for cfg in self.cfgs:
+            tree = DomTreeBuilder(cfg)
+            tree.build()
+            self.dom_trees.append(tree)
+
     def lower(self):
         self.build_cfg()
+        self.build_trees()
 
 
 if __name__ == "__main__":
@@ -414,6 +593,14 @@ if __name__ == "__main__":
     from .binder import Binder
 
     text = """
+fn test() {
+    print("test");
+    let a = 0;
+    if a == 0 {
+        a += 1;
+    }
+    return a;
+}
 let a = 1+1;
 a = 2;
 print(a+1);
@@ -432,12 +619,20 @@ if (a == 2 && a == 2) {
     mir = Mir(bound, debug=True)
     lowered = mir.lower()
     for cfg in mir.cfgs:
+        print(f"NEW CFG: {cfg.fn.sym.name}")
         for block in cfg.blocks:
-            print()
             print(f"BLOCK: {block.id}")
+            tmp = "PREDS: ["
+            for pred in block.preds:
+                tmp += str(pred.id) + ", "
+            if len(block.preds) > 0:
+                tmp = tmp[0:-2]
+            tmp += "]"
+            print(tmp)
             print("STMTS:")
             for stmt in block.stmts:
                 print(stmt)
             print("TERM:")
             print(block.term)
             print()
+        print()
